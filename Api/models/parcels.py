8e4989dc3941fc50a6437d0ec3b56e5724653d20
anyatibrian <anyatibrian@gmail.com>
2018-11-06 15:16:41 +0300

    calls the serial number generator in parcel class[finishes #161744575]

--- Api/models/parcels.py
+++ Api/models/parcels.py
@@ -1,18 +1,19 @@
-import random
+from Api.utilities import serial_generator
 
 parcel_orders = []
 
 
 class ParcelOrders:
     """class responsible for creating the parcel orders"""
+
     def __init__(self):
         pass
 
     def create_orders(self, parcel_name, description, pick_up, destination, status="pending"):
         """function creates the parcel order list """
-        serial = serial_number()
+        serial = serial_generator()
         parcel_order = {
-            'parcel_id': len(parcel_orders)+1,
+            'parcel_id': len(parcel_orders) + 1,
             'parcel_name': parcel_name,
             'description': description,
             'pick-up': pick_up,
@@ -21,4 +22,4 @@ class ParcelOrders:
             'serial_no': serial
         }
         parcel_orders.append(parcel_order)
-        return parcel_order
+        return parcel_orders
