e34dbbf1fe6aeaa862e2af651495ad67d74f8087
anyatibrian <anyatibrian@gmail.com>
2018-11-06 15:36:06 +0300

    completes the create order endpoints[finishes #161744575]

--- Api/Api_v1/create_parcel_order_endpoints.py
+++ Api/Api_v1/create_parcel_order_endpoints.py
@@ -1,7 +1,19 @@
 from flask import request, jsonify
 from ..Api_v1 import api_v1
+from ..models.parcels import ParcelOrders
 
 
 @api_v1.route('/parcels', methods=['POST'])
 def post_parcels():
-    return jsonify({'message': 'parcel order page'})
+    # creating the json request
+    json_data = request.get_json(force=True)
+
+    parcel_order = ParcelOrders()
+
+    # call to the create parcel methods
+    parcel_order.create_orders(parcel_name=json_data['parcel_name'],
+                               description=json_data['description'],
+                               pick_up=json_data['pick_up'],
+                               destination=json_data['destination'])
+
+    return jsonify({'message': 'parcel delivery orders created successfully'}), 201
