2948aeadc28dbf3cd690550232e497e3b02e5736
anyatibrian <anyatibrian@gmail.com>
2018-11-07 16:10:24 +0300

    validates order status  [finishes #161776751]

--- Api/Api_v1/update_parcel_order_status_endpoints.py
+++ Api/Api_v1/update_parcel_order_status_endpoints.py
@@ -8,4 +8,4 @@ def put_parcel_delivery_status(parcel_id):
     json_data = request.get_json(force=True)
     delivery_order = [delivery_order for delivery_order in parcel_orders if delivery_order['parcel_id'] == parcel_id]
     delivery_order[0]['status'] = json_data['status']
-    return jsonify({'message': 'your order has been successfully updated'}), 200
+    return jsonify({'message': 'your order has been successfully updated'}), 201
