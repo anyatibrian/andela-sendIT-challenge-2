da28c82fbe5e345f8ce4fac0c7d71c27a07afa27
anyatibrian <anyatibrian@gmail.com>
2018-11-07 12:26:28 +0300

    adds test for get single  parcel order [finishes #161745881]

--- Api/Api_v1/get_single_delivery_order_endpoints.py
+++ Api/Api_v1/get_single_delivery_order_endpoints.py
@@ -8,6 +8,6 @@ def get_single_parcel_order(parcel_id):
     """function that fetches a single order from the list"""
     if 0 < parcel_id < len(parcel_orders):
         parcel_order = [parcel_order for parcel_order in parcel_orders if parcel_order['parcel_id'] == parcel_id]
-        return jsonify({'parcel_order': parcel_order[0]}), 201
+        return jsonify({'parcel_order': parcel_order[0]}), 200
     else:
-        return jsonify({'message': 'you dont have such a parcel order'})
+        return jsonify({'message': 'you dont have such a parcel order'}), 200
