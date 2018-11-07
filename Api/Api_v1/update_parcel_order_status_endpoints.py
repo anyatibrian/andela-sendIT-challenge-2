9e3636193a20670445b531b94c1a4ab5184a017a
anyatibrian <anyatibrian@gmail.com>
2018-11-07 15:56:20 +0300

    add  put_parcel_delivery_status to the endpoints [finishes #161776751]

---
+++ Api/Api_v1/update_parcel_order_status_endpoints.py
@@ -0,0 +1,11 @@
+from flask import request, jsonify
+from ..Api_v1 import api_v1
+from ..models.parcels import parcel_orders
+
+
+@api_v1.route('parcels/<int:parcel_id>', methods=['PUT'])
+def put_parcel_delivery_status(parcel_id):
+    json_data = request.get_json(force=True)
+    delivery_order = [delivery_order for delivery_order in parcel_orders if delivery_order['parcel_id'] == parcel_id]
+    delivery_order[0]['status'] = json_data['status']
+    return jsonify({'message': 'your order has been successfully updated'}), 200
