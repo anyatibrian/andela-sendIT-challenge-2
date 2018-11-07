3d57c9100337c2b20fb0c4d48080b162ae4a759c
anyatibrian <anyatibrian@gmail.com>
2018-11-07 12:07:09 +0300

    adds validation checks for invalid id [finishes #161745881]

--- Api/Api_v1/get_single_delivery_order_endpoints.py
+++ Api/Api_v1/get_single_delivery_order_endpoints.py
@@ -6,5 +6,8 @@ from ..models.parcels import parcel_orders
 @api_v1.route('parcels/<int:parcel_id>', methods=['GET'])
 def get_single_parcel_order(parcel_id):
     """function that fetches a single order from the list"""
-    parcel_order = [parcel_order for parcel_order in parcel_orders if parcel_order['parcel_id'] == parcel_id]
-    return jsonify({'parcel_order': parcel_order[0]}), 201
+    if 0 < parcel_id < len(parcel_orders):
+        parcel_order = [parcel_order for parcel_order in parcel_orders if parcel_order['parcel_id'] == parcel_id]
+        return jsonify({'parcel_order': parcel_order[0]}), 201
+    else:
+        return jsonify({'message': 'you dont have such a parcel order'})
