c84687f5c42028cab8bd210e6dbec109ca087e7a
anyatibrian <anyatibrian@gmail.com>
2018-11-07 11:38:19 +0300

    adds function for getting single product [finishes #161745881]

---
+++ Api/Api_v1/get_single_delivery_order_endpoints.py
@@ -0,0 +1,10 @@
+from flask import jsonify
+from ..Api_v1 import api_v1
+from ..models.parcels import parcel_orders
+
+
+@api_v1.route('parcels/<int:parcel_id>', methods=['GET'])
+def get_single_parcel_order(parcel_id):
+    """function that fetches a single order from the list"""
+    parcel_order = [parcel_order for parcel_order in parcel_orders if parcel_order['parcel_id'] == parcel_id]
+    return jsonify({'parcel_order': parcel_order[0]}), 201
