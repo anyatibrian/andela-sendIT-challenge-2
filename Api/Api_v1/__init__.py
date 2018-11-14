8be01f7dbb54707ba47c77f765449ce65d459755
anyatibrian <anyatibrian@gmail.com>
2018-11-14 12:23:12 +0300

    combines the get all and get single endpoint into one file

--- Api/Api_v1/__init__.py
+++ Api/Api_v1/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 # creating the blueprint for our app
 api_v1 = Blueprint('api_v1', __name__)
-from .import create_parcel_order_endpoints, get_all_parcel_delivery_order_endpoints,\
-    get_single_delivery_order_endpoints, update_parcel_order_status_endpoints
+from .import create_parcel_order_endpoints, get_parcel_delivery_order_endpoints,\
+  update_parcel_order_status_endpoints
--- Api/Api_v1/get_parcel_delivery_order_endpoints.py
+++ Api/Api_v1/get_parcel_delivery_order_endpoints.py
@@ -3,6 +3,15 @@ from ..Api_v1 import api_v1
 from ..models.parcels import parcel_orders
 
 
+@api_v1.route('/parcels', methods=['GET'])
+def get_all_parcel_orders():
+    """function that get all the parcel delivery order"""
+    if len(parcel_orders) != 0:
+        return jsonify({'parcel_orders': parcel_orders})
+    else:
+        return jsonify({'message': 'your parcel order list is empty'})
+
+
 @api_v1.route('parcels/<int:parcel_id>', methods=['GET'])
 def get_single_parcel_order(parcel_id):
     """function that fetches a single order from the list"""
@@ -10,4 +19,4 @@ def get_single_parcel_order(parcel_id):
         parcel_order = [parcel_order for parcel_order in parcel_orders if parcel_order['parcel_id'] == parcel_id]
         return jsonify({'parcel_order': parcel_order[0]}), 200
     else:
-        return jsonify({'message': 'you dont have such a parcel order'}), 200
+        return jsonify({'message': 'parcel order does not exist'}), 200
