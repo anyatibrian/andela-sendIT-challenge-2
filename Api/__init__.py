6d02c0f53d721c61448a1216da4c3930ef4c2e90
anyatibrian <anyatibrian@gmail.com>
2018-11-15 12:13:40 +0300

    creates and initializes the jwt Mangaer object used in session management[finishes#161950465]

--- Api/__init__.py
+++ Api/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import config
+from flask_jwt_extended import JWTManager
 
 
 def create_app(config_name):
     """the create app method that creates an app instance"""
     app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = 'no-form-against-me-shall-prosper'
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    # initializes jwt object
+    jwt = JWTManager(app)
     app.config.from_object(config[config_name])
     # registering the blue print object
     from Api.Api_v1 import api_v1
